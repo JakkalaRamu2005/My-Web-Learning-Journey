08395a9d941356c6701eba601d9f029502dd4863
JakkalaRamu2005 <jakkalaramu233@gmail.com>
2025-10-26 10:38:21 +0530

    updated clock

--- javascriptcoding/js-projects/clock/index.js
+++ javascriptcoding/js-projects/clock/index.js
@@ -3,14 +3,36 @@ function showTime(){
 
     const time = new Date();
 
-    let hr = time.getHours();
-    let min = time.getMinutes();
-    let sec = time.getSeconds();
-    let milliSec = time.getMilliseconds();
+    let hours = time.getHours();
+    let minutes = time.getMinutes();
+    let seconds = time.getSeconds();
 
-    console.log(hr);
-    console.log(min);
-    console.log(sec);
+
+    let ampm = hours>=12 ? "pm": "am";
+if(hours>12){
+    hours = hours-12;
+}
+
+if(hours==0){
+    hours = 12;
+}
+
+hours = hours<10 ? '0'+hours: hours;
+minutes = minutes<10 ? '0'+minutes:minutes;
+seconds  = seconds <10 ? '0'+seconds: seconds;
+
+
+
+
+    let currentTime = hours + ":" + minutes + ":" + seconds + " " + ampm;
+
+
+    document.getElementById("clock").textContent = currentTime;
+
+
+    setInterval(showTime, 1000);
+
+
 }
 
 showTime();
